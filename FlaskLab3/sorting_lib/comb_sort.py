c1dd4a1c06733277f65cac22d6a943f9a264432d
Николай <@DESKTOP-VLDU8FO>
2024-12-19 23:03:45 +0300

    Добавьте файлы проекта.

---
+++ FlaskLab3/sorting_lib/comb_sort.py
@@ -0,0 +1,18 @@
+﻿# Функция сортировки Расчёской
+def comb_sort(arr):
+    
+    jump = len(arr)
+    fact = 1.25
+    swapped = True
+    
+    while jump >= 1 or swapped:
+        swapped = False
+        
+        for i in range (len(arr) - jump):
+            if arr[i] > arr[i+jump]:
+                arr[i], arr[i+jump] = arr[i+jump], arr[i]
+                swapped = True
+                
+        jump = (int)(jump / fact);
+
+    return arr
